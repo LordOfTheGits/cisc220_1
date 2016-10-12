43716cb6a0ebc1f75b32974ac9966fd004d5de98
mattrodgers <15mmr@queensu.ca>
2016-10-11 22:17:42 -0400

    Add files via upload
    Bugfixes & cleanup

--- arithmetic.sh
+++ arithmetic.sh
@@ -3,55 +3,68 @@ result=0
 num=""
 firstrun=1
 while [[ true ]]; do
-	badNum=0
-	read -p "Enter a number" num
+	invaildNum=0
+	read -p "Enter a number: " num
 	if [ ${#num} -eq 0 ]; then
 		echo error
 		continue
 	fi
+	
 	if [ $num == ':q' ]; then
 		break
 	fi
+
 	for (( i = 0; i < ${#num}; i++ )); do
 		case ${num:$i:1} in
 			[0-9])
 				;;
 			*)
 				echo error
-				badNum=1
+				invaildNum=1
 				break
 				;;
 		esac
 	done
-	if [ $badNum -eq 1 ]; then
-		badNum=0
+
+	if [ $invaildNum -eq 1 ]; then
+		invaildNum=0
 		continue
 	fi
+
 	if [ $firstrun -eq 1 ]; then
 		result=$num
 		firstrun=0
-		continue
+	else
+		result=$(($result $opperator $num))
+		echo The result so far is $result
 	fi
+
 	opperator=""
-	read -p "Enter an opperator" opperator
-	case $opperator in
-		':q')
-			break
-			;;
-		'*')
-			;;
-		/)
-			;;
-		+)
-			;;
-		-)
-			;;
-		*)
-			echo Error
-			continue
-			;;
-		esac
-	result=$(($result $opperator $num))
-	echo The result so far is $result
+	invaildOpp=1
+	while [[ $invaildOpp -eq 1 ]]; do
+		read -p "Enter an operator: " opperator
+		case $opperator in
+			':q')
+				echo 'Thanks for using my calculator'
+				exit
+				;;
+			'*')
+				invaildOpp=0
+				;;
+			/)
+				invaildOpp=0
+				;;
+			+)
+				invaildOpp=0
+				;;
+			-)
+				invaildOpp=0
+				;;
+			*)
+				echo Error
+				continue
+				;;
+			esac
+	done
 done
 echo 'Thanks for using my calculator'
